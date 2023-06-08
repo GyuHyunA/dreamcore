f15cd79218e5a8a20646fc45977d7e51e82cfdad
GyuHyunA <ghahn97@gmail.com>
2023-06-09 05:12:50 +0900

    about 페이지 애니메이션 추가

--- src/components/about/aboutHome.jsx
+++ src/components/about/aboutHome.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 const AboutHome = () => {
+  const [tar, setTar] = useState(0);
+  const [btar, setbtar] = useState(false);
+
+  function onScroll() {
+    setTar(window.scrollY);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    if (tar > 0) {
+      setbtar(true);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [tar, btar]);
+
+  console.log(tar);
+
   return (
     <>
       <AboutHomeStyle>
-        <h1>DigitalContentsDesign</h1>
-        <h2>디지털콘텐츠디자인</h2>
-        <p>현존하는 미디어를 통해서 발신하는 방법을 배우고</p>
-        <p>창작하는 과정에서 다양한 표현 방식을 배워 나갑니다</p>
+        <h1 className={`${btar ? "act" : ""}`}>DigitalContentsDesign</h1>
+        <h2 className={`${btar ? "act" : ""}`}>디지털콘텐츠디자인</h2>
+        <p className={`${btar ? "act" : ""}`}>현존하는 미디어를 통해서 발신하는 방법을 배우고</p>
+        <p className={`${btar ? "act" : ""}`}>창작하는 과정에서 다양한 표현 방식을 배워 나갑니다</p>
       </AboutHomeStyle>
       <AboutProStyle>
         <h2>Project Guidance Professor</h2>
@@ -47,13 +67,29 @@ const AboutHomeStyle = styled.section`
   h1 {
     font-size: 40px;
     margin-bottom: 10px;
+    opacity: 0;
+    transition: 1s;
+    &.act {
+      opacity: 1;
+    }
   }
   h2 {
     font-size: 18px;
     margin-bottom: 50px;
+    opacity: 0;
+    transition: 1s;
+    &.act {
+      opacity: 1;
+    }
   }
   p {
     font-size: 12px;
+    opacity: 0;
+    transition: 1s;
+    transition-delay: 2s;
+    &.act {
+      opacity: 1;
+    }
   }
 `;
 
