a226f66205b10ce34c081ec2ee51923414911fc0
GyuHyunA <ghahn97@gmail.com>
2023-09-26 12:39:10 +0900

    pc profile 수정

--- src/components/pc/profile/profileHome.jsx
+++ src/components/pc/profile/profileHome.jsx
@@ -47,15 +47,16 @@ const ListContainer = ({ kname, ename, email, profile }) => {
   return (
     <li>
       <Link to={""} style={{ cursor: "auto" }}>
-        <div>
+        <div className="list-wrap">
+          <div className="name-box">
+            <div className="filter"></div>
+            <h1 className="kname">{kname}</h1>
+            <p className="ename">{ename}</p>
+            <p className="email">{email}</p>
+          </div>
           <img src={`${process.env.PUBLIC_URL}/assets/profileimg/${comName}.${"jpg"}`} alt="" />
         </div>
       </Link>
-      <div className="name-box">
-        <h1 className="kname">{kname}</h1>
-        <p className="ename">{ename}</p>
-        <p className="email">{email}</p>
-      </div>
     </li>
   );
 };
@@ -105,38 +106,45 @@ const ContentsListStyle = styled.ul`
     height: 300px;
     list-style: none;
     position: relative;
-    a div {
+    a .list-wrap {
       width: 250px;
       height: 300px;
-      background-color: lightgray;
-      border-radius: 10px;
+      /* background-color: lightgray; */
+      .name-box {
+        padding: 20px 0;
+        padding-left: 15px;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        opacity: 0;
+        transition: 0.5s;
+        &:hover {
+          opacity: 1;
+        }
+        p {
+          font-size: 16px;
+        }
+        .kname {
+          font-size: 16px;
+          margin-bottom: 10px;
+        }
+        .ename,
+        .email {
+          font-family: "Pretendard-l";
+          color: #3b3b3b;
+        }
+        .filter {
+          position: absolute;
+          width: 250px;
+          height: 300px;
+          bottom: 0;
+          left: 0;
+          background-color: #00000020;
+        }
+      }
       img {
         width: inherit;
         height: inherit;
-      }
-    }
-    .name-box {
-      padding: 20px 0;
-      padding-left: 15px;
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      opacity: 0;
-      transition: 0.5s;
-      &:hover {
-        opacity: 1;
-      }
-      p {
-        font-size: 16px;
-      }
-      .kname {
-        font-size: 16px;
-        margin-bottom: 10px;
-      }
-      .ename,
-      .email {
-        font-family: "Pretendard-l";
-        color: #3b3b3b;
       }
     }
   }
